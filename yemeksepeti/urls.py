3a34ef2977c63c4e1478de6a7b389e869813a711
sibelkahraman <sibelkahraman235@gmail.com>
2021-12-29 18:52:44 +0300

    yemeksepeti project is developed

---
+++ yemeksepeti/urls.py
@@ -0,0 +1,33 @@
+"""yemeksepeti URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url, include
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Yemeksepetii Api')
+
+urlpatterns = [
+    url(r'^$', schema_view),
+    path('admin/', admin.site.urls),
+    url(r'addresses', include('app.address.urls')),
+    url(r'categories', include('app.category.urls')),
+    url(r'foods', include('app.food.urls')),
+    url(r'ingredients', include('app.ingredient.urls')),
+    path(r'orders', include('app.order.urls')),
+    url(r'restaurants', include('app.restaurant.urls')),
+    url(r'users', include('app.user.urls')),
+]
